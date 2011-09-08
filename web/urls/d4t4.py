9862485b3ceaddf1472fcc17ac80c434271eb160
David Joerg <dsjoerg@gmail.com>
2011-09-08 20:52:47 +0400

    highlight the line that kills DJs mac

--- web/urls/d4t4.py
+++ web/urls/d4t4.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
 
     (r'chart/', include('chart.urls')),
 
+# the following line causes UnicodeEncodeError on DJs Mac. crazy.
     (ur'^(?P<sparkblocks>[▁▂▃▅▆▇]+)$', 'chart.views.sparklink'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
