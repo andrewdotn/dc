0531acc7953d69256d2b19f63cb9af5a9b6cbe53
Mark Smith <mark@camazotz.com>
2011-09-13 23:40:10 +0400

    'Internal' query item on embed url.

--- web/chart/views.py
+++ web/chart/views.py
@@ -76,4 +76,4 @@ def convert(request, chart_id):
 def embed(request, chart_id):
     chart = get_object_or_404(Chart, id=chart_id)
 
-    return render(request, 'chart/embed.html', {'host': request.get_host(), 'chart': chart})
+    return render(request, 'chart/embed.html', {'host': request.get_host(), 'chart': chart, 'internal': bool(request.GET.get("internal", False))})
