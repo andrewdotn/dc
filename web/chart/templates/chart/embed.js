39b053ce3253b5b2688cfbdc154a838a22675d6f
David Joerg <dsjoerg@gmail.com>
2011-09-19 21:52:55 +0400

    layout improvements for Share panel and embedded chart. fixed hideous scrollbar layout problem

--- web/chart/templates/chart/embed.js
+++ web/chart/templates/chart/embed.js
@@ -6,7 +6,7 @@
   new DATACOLLECTIVE.easyXDM.Socket({
     remote: "{{chart_url}}",
     container: document.getElementById("datacollective-chart-{{chart.id}}"),
-    props: { style: { width: "630px", height: "500px" } },
+    props: { style: { width: "630px", height: "400px" } },
     onMessage: function(message, origin) {
       this.container.getElementsByTagName("iframe")[0].style.height = message + "px";
     }
