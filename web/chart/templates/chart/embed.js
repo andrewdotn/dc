88b3848027be4fa56cb65e5ff1d2d9ba1fe73208
Mark Smith <mark@camazotz.com>
2011-09-19 17:51:23 +0400

    JS to generate embedded chart.

---
+++ web/chart/templates/chart/embed.js
@@ -0,0 +1,14 @@
+(function() {
+  if (typeof(DATACOLLECTIVE) === 'undefined') {
+    DATACOLLECTIVE = { easyXDM: easyXDM.noConflict("DATACOLLECTIVE") };
+  }
+
+  new DATACOLLECTIVE.easyXDM.Socket({
+    remote: "{{chart_url}}",
+    container: document.getElementById("datacollective-chart-{{chart.id}}"),
+    props: { style: { width: "630px", height: "500px" } },
+    onMessage: function(message, origin) {
+      this.container.getElementsByTagName("iframe")[0].style.height = message + "px";
+    }
+  });
+})();
