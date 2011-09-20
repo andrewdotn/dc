1560c9c3573e58e036069fe0f7dc180c478af870
Andrew Neitsch <andrew@subjoin.net>
2011-09-21 01:16:49 +0400

    Add changes needed for automatic deployment.

--- web/common/deploy/passenger_wsgi.py
+++ web/common/deploy/passenger_wsgi.py
@@ -11,8 +11,6 @@ if sys.version < "2.7":
   python = os.path.join(os.environ['HOME'], 'local', 'bin', 'python2.7')
   os.execl(python, python, *sys.argv)
 
-
-
 # On linux, toggle this to find out where stdout and stderr logs are going
 # before redirection. stdout is fd 1, stderr is fd 2.
 if False:
@@ -39,7 +37,7 @@ resource.setrlimit(resource.RLIMIT_CORE, (0, 0))
 import django.core.handlers.wsgi
 sys.path.insert(0, os.path.join(os.getcwd(), 'dc/web'))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = "settings.d4t4_prod"
+os.environ['DJANGO_SETTINGS_MODULE'] = "@settings@"
 
 def application(environ, start_response):
     try:
--- web/settings/dc_dev.py
+++ web/settings/dc_dev.py
@@ -1,2 +1,2 @@
 from development import *
-from datacollective import *
+from dc import *
