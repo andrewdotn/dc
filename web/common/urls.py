2f7bb7b962384ec9a9dcb4ea50ed459912fb45f5
Andrew Neitsch <andrew@subjoin.net>
2011-10-20 23:20:51 +0600

    Update 404 page to use base template.

--- web/common/urls.py
+++ web/common/urls.py
@@ -8,8 +8,11 @@ from django.views.decorators.csrf import requires_csrf_token
 
 @requires_csrf_token
 def handler404(request):
-  d = {'request_path': request.path}
-  t, e, traceback = sys.exc_info()
-  if t == Http404:
-    d['exception'] = e
-  return render(request, '404.html', d, status=404)
+    d = {
+        'request_path': request.path,
+        'referer': request.META.get('HTTP_REFERER', '')
+    }
+    t, e, traceback = sys.exc_info()
+    if t == Http404:
+        d['exception'] = e
+    return render(request, '404.html', d, status=404)
