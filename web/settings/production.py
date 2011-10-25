5fa5327296626aecedda840d615facd43dfa5ce2
David Joerg <dsjoerg@gmail.com>
2011-10-25 21:44:30 +0400

    backing out CDN/STATIC_URL changes that we're not ready for yet

--- web/settings/production.py
+++ web/settings/production.py
@@ -14,8 +14,6 @@ DATA_DIR = os.path.expanduser("~/data")
 
 SENTRY_URL_PREFIX = 'https://datacollective.org/'
 
-STATIC_URL = 'https://d9zwdpgl8r5wd.cloudfront.net/'
-
 # Cache compiled templates. Requires all template tags to be thread-safe.
 # https://docs.djangoproject.com/en/1.3/howto/custom-template-tags/#template-tag-thread-safety
 TEMPLATE_LOADERS = (
